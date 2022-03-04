786d48bfa7d014906543f751a094d070c34060ab
j-code <jmjuuj@utu.fi>
2022-03-04 16:46:10 +0200

    Cloned a new JavaFX-project template.

---
+++ src/test/java/fi/utu/tech/gui/javafx/DummyTest.java
@@ -0,0 +1,13 @@
+package fi.utu.tech.gui.javafx;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class DummyTest {
+
+    @Test
+    public void dummy() throws Exception {
+        assertTrue(true);
+    }
+}
