66c56f84f56465a18e27a024ca874f315c964b87
j-code <jmjuuj@utu.fi>
2022-03-19 23:12:54 +0200

    Restructured and simplified the Sprite class.

--- src/main/java/fi/utu/tech/gui/javafx/Sprite.java
+++ src/main/java/fi/utu/tech/gui/javafx/Sprite.java
@@ -7,64 +7,79 @@ import javafx.scene.image.ImageView;
 
 public class Sprite extends AnimationTimer {
 
-
     private final ImageView imageView; //Image view that will display our sprite
 
     private final int totalFrames; //Total number of frames in the sequence
     private final float fps; //frames per second I.E. 24
-
     private final int cols; //Number of columns on the sprite sheet
-    private final int rows; //Number of rows on the sprite sheet
-
-    private final int frameWidth; //Width of an individual frame
-    private final int frameHeight; //Height of an individual frame
-
-    private int currentCol = 0;
-    private int currentRow = 0;
-
+    private int frameWidth; //Width of an individual frame
+    private int frameHeight; //Height of an individual frame
+    private final int repeat;
+    private int repeatCounter;
+    private int currentFrame = 0;
     private long lastFrame = 0;
 
-    public Sprite(ImageView imageView, Image image, int columns, int rows, int totalFrames, int frameWidth, int frameHeight, float framesPerSecond) {
+    
+    public Sprite(ImageView imageView, Image image, int columns, int rows, int frameWidth,
+    		int frameHeight, float framesPerSecond, int repeat) {
+    	
         this.imageView = imageView;
         imageView.setImage(image);
-        imageView.setViewport(new Rectangle2D(0, 0, frameWidth, frameHeight));
-
+        imageView.setVisible(false);
         cols = columns;
-        this.rows = rows;
-        this.totalFrames = totalFrames;
+        this.totalFrames = columns * rows;
         this.frameWidth = frameWidth;
         this.frameHeight = frameHeight;
         fps = framesPerSecond;
-
+        this.repeat = repeat; /* -1 = infinite repeat*/
+        this.repeatCounter = repeat;
+        showFrame(currentFrame);
+    }
+    
+    @Override
+    public void start() {
         lastFrame = System.nanoTime();
+        imageView.setVisible(true);
+    	super.start();
     }
 
     @Override
     public void handle(long now) {
-        int frameJump = (int) Math.floor((now - lastFrame) / (1000000000 / fps)); //Determine how many frames we need to advance to maintain frame rate independence
-
-        //Do a bunch of math to determine where the viewport needs to be positioned on the sprite sheet
+    	if (repeatCounter == 0) {
+            this.repeatCounter = repeat;
+    		this.stop();
+            imageView.setVisible(false);
+    		return;
+    	}
+    	
+    	//Determine how many frames we need to advance to maintain frame rate independence
+        int frameJump = (int) Math.floor((now - lastFrame) / (1000000000 / fps));
+        
         if (frameJump >= 1) {
             lastFrame = now;
-            int addRows = (int) Math.floor((float) frameJump / (float) cols);
-            int frameAdd = frameJump - (addRows * cols);
-
-            if (currentCol + frameAdd >= cols) {
-                currentRow += addRows + 1;
-                currentCol = frameAdd - (cols - currentCol);
-            } else {
-                currentRow += addRows;
-                currentCol += frameAdd;
-            }
-            currentRow = (currentRow >= rows) ? currentRow - ((int) Math.floor((float) currentRow / rows) * rows) : currentRow;
-
-            //The last row may or may not contain the full number of columns
-            if ((currentRow * cols) + currentCol >= totalFrames) {
-                currentRow = 0;
-                currentCol = Math.abs(currentCol - (totalFrames - (int) (Math.floor((float) totalFrames / cols) * cols)));
-            }
-
-            imageView.setViewport(new Rectangle2D(currentCol * frameWidth, currentRow * frameHeight, frameWidth, frameHeight));
+            currentFrame += frameJump;
+            repeatCounter -= currentFrame / totalFrames;
+            currentFrame %= totalFrames;
+            showFrame(currentFrame);
         }
+    }
+    
+    public void showFrame(int frameNumber) {
+    	final int col = frameNumber % cols;
+    	final int row = frameNumber / cols;
+    	imageView.setViewport(new Rectangle2D(col * frameWidth, row * frameHeight, frameWidth, frameHeight));
+    }
+    
+    public void showNextFrame() {
+    	currentFrame++;
+        currentFrame %= totalFrames;
+    	showFrame(currentFrame);
+    }
+    
+    public void scaleBy(double scaleFactor) {
+    	imageView.setScaleX(scaleFactor);
+    	imageView.setScaleY(scaleFactor);
+    	frameWidth *= frameWidth;
+    	frameHeight *= frameHeight;
     }
 }
