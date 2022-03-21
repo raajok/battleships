c8e2e9fee727f748ab1dcef3a84fc588103aa01d
Kasperi Aavasaari <KasperiA@users.noreply.github.com>
2022-03-21 20:31:25 +0200

    more changes

--- src/main/java/fi/utu/tech/gui/javafx/setShipsSceneController.java
+++ src/main/java/fi/utu/tech/gui/javafx/setShipsSceneController.java
@@ -1,9 +1,13 @@
 package fi.utu.tech.gui.javafx;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Line;
 import javafx.scene.text.Text;
@@ -25,24 +29,64 @@ public class setShipsSceneController {
 	@FXML
 	private Text playerText;
 	
+	@FXML
+	private HBox topBox;
+	
+	@FXML
+	private HBox bottomBox;
+	
+	@FXML
+	private ImageView battleship;
+	
+	@FXML
+	private ImageView carrier;
+	
+	@FXML
+	private ImageView cruiser;
+	
+	@FXML
+	private ImageView destroyer;
+	
+	@FXML
+	private ImageView submarine;
+	
 	private BattleshipGame game;
+	private int gameboardSize;
 	
 	@FXML
 	public void initialize() {
-		this.game = new BattleshipGame();
+		this.game = MainApp.getGame();
+		this.gameboardSize = game.boardSizeProperty().get();
 		
-		for(int i = 0; i < 11/*gameboardSize + 1*/; i++) {
+		for(int i = 0; i < gameboardSize + 1; i++) {
 			System.out.println(boardPane.getMaxWidth());
 			Line verticaLine = new Line();
-			verticaLine.startXProperty().bind(boardPane.widthProperty().divide(10/*gameboardSize*/).multiply(i));
-			verticaLine.endXProperty().bind(boardPane.widthProperty().divide(10/*gameboardSize*/).multiply(i));
+			verticaLine.startXProperty().bind(boardPane.widthProperty().divide(this.gameboardSize).multiply(i));
+			verticaLine.endXProperty().bind(boardPane.widthProperty().divide(this.gameboardSize).multiply(i));
 			verticaLine.endYProperty().bind(boardPane.heightProperty());
 			Line horizontalLine = new Line();
-			horizontalLine.startYProperty().bind(boardPane.heightProperty().divide(10/*gameboardSize*/).multiply(i));
-			horizontalLine.endYProperty().bind(boardPane.heightProperty().divide(10/*gameboardSize*/).multiply(i));
+			horizontalLine.startYProperty().bind(boardPane.heightProperty().divide(this.gameboardSize).multiply(i));
+			horizontalLine.endYProperty().bind(boardPane.heightProperty().divide(this.gameboardSize).multiply(i));
 			horizontalLine.endXProperty().bind(boardPane.widthProperty());
 			boardPane.getChildren().add(verticaLine);
 			boardPane.getChildren().add(horizontalLine);
+			
+			Image battleshipImage = new Image(getClass().getResource("Battleship_placeholder.png").toExternalForm());
+			battleship.setImage(battleshipImage);
+			
+			Image carrierImage = new Image(getClass().getResource("Carrier_placeholder.png").toExternalForm());
+			carrier.setImage(carrierImage);
+			
+			Image cruiserImage = new Image(getClass().getResource("Cruiser_placeholder.png").toExternalForm());
+			cruiser.setImage(cruiserImage);
+			
+			Image destroyerImage = new Image(getClass().getResource("Destroyer_placeholder.png").toExternalForm());
+			destroyer.setImage(destroyerImage);
+			
+			Image submarineImage = new Image(getClass().getResource("Submarine_placeholder.png").toExternalForm());
+			submarine.setImage(submarineImage);
+			
+			
 		}
 	}
 	
@@ -51,9 +95,14 @@ public class setShipsSceneController {
 		// koko ruudun koko
 		System.out.println("outerpane width: " + outerPane.getWidth()); 
 		System.out.println("outerpane height: " + outerPane.getHeight());
+		System.out.println("topbox height: " + topBox.getHeight());
+		System.out.println("topbox height: " + topBox.getWidth());
+		System.out.println("bottombox height: " + bottomBox.getHeight());
+		System.out.println("bottombox height: " + bottomBox.getWidth());
 		//peliruudukon panen koko
 		System.out.println("width:" + boardPane.getWidth());
 		System.out.println("height:" + boardPane.getHeight());
+		//top h
 	};
 	
 	@FXML
@@ -63,9 +112,22 @@ public class setShipsSceneController {
 	
 	@FXML
 	void printCoords(MouseEvent e) {
-		int rectangleX = (int) Math.floor(e.getX() / boardPane.getWidth() * 10/*gameboardSize*/);
-		int rectangleY = (int) Math.floor(e.getY() / boardPane.getHeight() * 10/*gameboardSize*/);
+		int rectangleX = (int) Math.floor(e.getX() / boardPane.getWidth() * this.gameboardSize);
+		int rectangleY = (int) Math.floor(e.getY() / boardPane.getHeight() * this.gameboardSize);
 		playerText.setText(/*"x:" + e.getX() + " y:" + e.getY() + */" ruutu: (" + rectangleX + " : " + rectangleY + ")");
 	}
+	
+	@FXML
+	void moveShip() {
+		
+	}
+	
+	EventHandler<MouseEvent> moveship = new EventHandler<MouseEvent>() {
+		
+		@Override
+		public void handle(MouseEvent e) {
+			
+		}		
+	};
 
 }
